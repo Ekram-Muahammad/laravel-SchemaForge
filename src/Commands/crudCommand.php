fcb0a0be7de72551b24e61a2ae6c606e81fdcb1a
EkramAminTornet <114848248+EkramAminTornet@users.noreply.github.com>
2023-09-04 23:32:51 +0300

    adding check json file is valid or no

--- src/Commands/crudCommand.php
+++ src/Commands/crudCommand.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Ekram\SchemaForge\Commands;
+
 use Ekram\SchemaForge\Helpers\ApiController;
 use Ekram\SchemaForge\Helpers\Factory;
 use Ekram\SchemaForge\Helpers\ResoureController;
@@ -16,128 +17,131 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Ekram\SchemaForge\Helpers\Reset;
 
-class CrudCommand extends Command {
+class CrudCommand extends Command
+{
     protected $signature = 'make:crud {jsonFile} {action?}';
 
     protected $description = 'Create a Crud Operation';
 
-    public function handle() {
+    public function handle()
+    {
 
         $fields = [];
 
         $relationships = [];
 
-        $jsonFile = $this->argument( 'jsonFile' );
+        $jsonFile = $this->argument('jsonFile');
 
-        if ( file_exists( base_path( '/schemas/' . $jsonFile . '.json' ) ) ) {
+        if (file_exists(base_path('/schemas/' . $jsonFile . '.json'))) {
 
             try {
                 //code...
                 $conntection = DB::connection()->getPdo();
-            } catch ( \Throwable $th ) {
-                $this->error( 'The Database connection is  unavailable' );
+            } catch (\Throwable $th) {
+                $this->error('The Database connection is  unavailable');
                 return;
             }
 
-            $jsonFile = file_get_contents( base_path( '/schemas/' . $jsonFile . '.json' ) );
+            $jsonFile = file_get_contents(base_path('/schemas/' . $jsonFile . '.json'));
 
-            $jsonData = json_decode( $jsonFile, true );
+            if (Str::isJson($jsonFile)) {
+                $jsonData = json_decode($jsonFile, true);
 
-            $tableName = $jsonData[ 'tableName' ];
+                $tableName = $jsonData['tableName'];
 
-            $action = $this->argument( 'action' ) ?? 'none';
+                $action = $this->argument('action') ?? 'none';
 
-            if ( $action == 'reset' ) {
-                new Reset( $tableName );
+                if ($action == 'reset') {
+                    new Reset($tableName);
 
-                return;
-            } else if ( $action == 'update' ) {
-                new Reset( $tableName );
-            }
+                    return;
+                } else if ($action == 'update') {
+                    new Reset($tableName);
+                }
 
-           // if ( !Schema::hasTable( $tableName ) ) {
+                $modelName = Str::ucfirst($tableName);
 
-                $modelName = Str::ucfirst( $tableName );
-
-                $columns = $jsonData[ 'columns' ];
+                $columns = $jsonData['columns'];
 
                 $field = new Field();
-                $data = $field->generateFields( $columns );
+                $data = $field->generateFields($columns);
 
-                $fields = $data[ 'fields' ];
-                $relationships = $data[ 'relationships' ];
+                $fields = $data['fields'];
+                $relationships = $data['relationships'];
 
-                if ( !empty( $fields ) ) {
+                if (!empty($fields)) {
 
-                    if ($jsonData[ 'migration']) {
+                    if ($jsonData['migration']) {
                         $migrationController = new Migration();
-                        $migrationController->generateMigrationContent( $tableName, $fields );
+                        $migrationController->generateMigrationContent($tableName, $fields);
                     }
 
                     $modelController = new Model();
 
-                    $modelController->generateModel( $tableName, $fields, $modelName, $relationships );
+                    $modelController->generateModel($tableName, $fields, $modelName, $relationships);
 
                     // create seeder
 
-                    $createSeeder = $jsonData[ 'seeder' ] ?? false;
+                    $createSeeder = $jsonData['seeder'] ?? false;
 
-                    if ( $createSeeder ) {
-                        $numRows = $jsonData[ 'seederNumRows' ] ?? 10;
+                    if ($createSeeder) {
+                        $numRows = $jsonData['seederNumRows'] ?? 10;
 
                         // Generate the factory for the given table
 
                         $factory = new Factory();
 
-                        $factory->generateFactory( $tableName, Str::ucfirst( $tableName ), $fields );
+                        $factory->generateFactory($tableName, Str::ucfirst($tableName), $fields);
 
-                        $factory->addHasFactoryTrait( $tableName );
+                        $factory->addHasFactoryTrait($tableName);
 
                         // Generate the seeder file
-                        
+
                         $seeder = new Seeder();
-                        $seeder->generateSeederFile( $tableName );
+                        $seeder->generateSeederFile($tableName);
                         // Generate the seeder file content
-                        $seeder->generateSeederContent( $tableName, $fields, $numRows );
+                        $seeder->generateSeederContent($tableName, $fields, $numRows);
                     }
 
-                    $createController = $jsonData[ 'resourceController' ] ?? false;
+                    $createController = $jsonData['resourceController'] ?? false;
 
-                    $createBladeView = $jsonData[ 'views' ] ?? false;
+                    $createBladeView = $jsonData['views'] ?? false;
 
-                    if ( $createController || $createBladeView ) {
+                    if ($createController || $createBladeView) {
                         $resourceController = new ResoureController();
-                        $resourceController->generateController( $tableName, $fields, $modelName );
+                        $resourceController->generateController($tableName, $fields, $modelName);
                     }
 
-                    $createApiController = $jsonData[ 'apiController' ] ?? false;
+                    $createApiController = $jsonData['apiController'] ?? false;
 
-                    if ( $createApiController ) {
+                    if ($createApiController) {
                         $apiController = new ApiController();
-                        $apiController->generateApiController( $tableName, $fields, $modelName );
+                        $apiController->generateApiController($tableName, $fields, $modelName);
                     }
 
-                  
-                    if ( $createBladeView ) {
+
+                    if ($createBladeView) {
                         $views = new Views();
-                        $views->generateBladeViews( $tableName, $fields );
-                        $views->generateFormFields( $fields, $tableName );
+                        $views->generateBladeViews($tableName, $fields);
+                        $views->generateFormFields($fields, $tableName);
                     }
 
-                    Artisan::call( 'optimize:clear' );
+                    Artisan::call('optimize:clear');
 
-                    $this->info( 'all requirements created successfully' );
+                    $this->info('all requirements created successfully');
 
                 } else {
-                    $this->error( 'Not Fields provided' );
+                    $this->error('Not Fields provided');
                 }
-            //} 
-            // else {
-            //     $this->error( "The table $tableName exists in the database." );
-            // }
+
+            } else {
+                $this->error('The requested file is not valid');
+            }
+
+
 
         } else {
-            $this->error( 'The requested file is currently unavailable' );
+            $this->error('The requested file is currently unavailable');
         }
     }
 
